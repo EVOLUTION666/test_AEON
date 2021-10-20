c0e29d551e0df503712e80f0ff1378cd11cbba77
Andrey <nefedovandrey1997@gmail.com>
2021-10-20 18:11:48 +0300

    Initial Commit

---
+++ test_AEON/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test_AEON
+//
+//  Created by Andrey on 20.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
